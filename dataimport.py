46d34d7db07a93378ef2738e0005c586a15ea388
jacopojoy98 <jacopo.colombini@sns.it>
2025-01-21 15:48:02 +0100

    aggiunto un po' di dettagli sulla posizione dei punti critici

--- dataimport.py
+++ dataimport.py
@@ -1,16 +1,35 @@
 import torchvision
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import torch
 import os
+import numpy as np
 
 dataset = torchvision.datasets.MNIST(root = "Dataset",transform=torchvision.transforms.PILToTensor())
 sevens = []
 for data in dataset:
     if data[1]==7:
         sevens.append(data)
-os.mkdir("IMG")
-for i in range(12):
-    plt.imshow(sevens[i][0].squeeze(), cmap="gray")
-    plt.savefig("IMG/"+str(i)+".png")
-    plt.close()
 
+filename = "points.txt"
+points = []
+with open(filename) as f:
+    for line in f.readlines():
+        line = line.replace("(",""). replace(")","").split(",")
+        line =[int(s) for s in line]
+        p = [(line[i],line[i+1]) for i in range(len(line)//2)]
+        points.append((line[0],p))
+
+
+for i in range(45):    
+    for point in points:
+        if point[0]==i:
+            fig,ax = plt.subplots(i)
+            ax.imshow(sevens[i][0].squeeze(), cmap="gray")
+            for p in point[1]:
+                rect = patches.Rectangle(p,1,1,linewidth=0,edgecolor=None,color="red")
+                ax.add_patch(rect)
+            plt.savefig("IMG/"+str(i)+".png")
+            plt.close()
+           
+    
